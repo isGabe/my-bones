a218f53165334d5c3dcf8adf60a583fa47256b65
Gabriel Luethje <gabriel@thefstopdesign.com>
2013-01-11 21:03:48 -0700

    Basic CSS/SCSS changes: Add SASS silent selector mixins for grids. Get rid of .h1, .h2, etc CSS classes. Add gravity forms gravity.scss file.
    Also add Compass plugin rgbapng
    
    Signed-off-by: Gabriel Luethje <gabriel@thefstopdesign.com>

---
+++ config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+require '/Library/Ruby/Gems/1.8/gems/susy-1.0/lib/susy.rb'
+require '/Library/Ruby/Gems/1.8/gems/compass-rgbapng-0.1.1/lib/rgbapng.rb'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "library/css"
+sass_dir = "library/scss"
+images_dir = "library/images"
+javascripts_dir = "library/js"
+
+output_style = :nested
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 				<div id="inner-header" class="wrap clearfix">
 					
 					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<h1 id="logo" ><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
 					
 					<!-- if you'd like to use the site description you can un-comment it below -->
 					<?php // bloginfo('description'); ?>
